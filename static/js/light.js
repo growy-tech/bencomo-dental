6bf4c6d674abdc6ad1409cb6a45d6b88204ae25b
victorroddev <javier.rod.dev@gmail.com>
2025-02-07 22:01:52 -0700

    commit to update

--- static/js/light.js
+++ static/js/light.js
@@ -3,18 +3,22 @@ document.addEventListener('DOMContentLoaded', () => {
     const lightOn = document.getElementById('light-on');
     const space = document.getElementById('space');
     const paragraph = document.getElementById('light-paragraph');
+    const paragraphTwo = document.getElementById('light-paragraph-2')
 
     lightOff.addEventListener('click', () =>{
         lightOff.classList.toggle('hidden');
         lightOn.classList.toggle('hidden');
         space.classList.replace('bg-gray','bg-deepestBlue');
-        paragraph.classList.replace('text-lightBlue', 'text-deepestBlue');
+        paragraph.classList.toggle('hidden');
+        paragraphTwo.classList.toggle('hidden');
     })
 
     lightOn.addEventListener('click', () =>{
         lightOn.classList.toggle('hidden');
         lightOff.classList.toggle('hidden');
         space.classList.replace('bg-deepestBlue', 'bg-gray');
-        paragraph.classList.replace('text-deepestBlue', 'text-lightBlue');
+        paragraph.classList.toggle('hidden');
+        paragraphTwo.classList.toggle('hidden');
+        
     })
 })
