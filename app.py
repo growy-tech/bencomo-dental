b8bf8240764ca258001d029f4ac6d2a41592bd2c
victorroddev <javier.rod.dev@gmail.com>
2025-03-10 16:01:15 -0600

    TEST form

--- app.py
+++ app.py
@@ -18,46 +18,26 @@ def home():
 # Ruta para manejar el envío de correos
 @app.route('/web/mail', methods=['GET', 'POST'])
 def email():
-    if request.method == "POST":
-
-        turnstile_response = request.form.get('cd-turnstile-response')
-
-        validation_response = requests.post(
-            "https://challenges.cloudflare.com/turnstile/v0/siteverify",
-            data={
-                "secret": TURNSTILE_SECRET_KEY,
-                "response": turnstile_response
-            }
-        )
-
-        result = validation_response.json()
+    if request.method == 'POST':
+        token = request.form.get('cf-turnstile-response')
+        if not token:
+            return "error Turnslite, toke no found", 400
+        
+        payload = {
+            'secret': TURNSTILE_SECRET_KEY,
+            'response': token,
+            'remoteip': request.remote_addr
+        }
+        
+        response = requests.post('https://challenges.cloudflare.com/turnstile/v0/siteverify', data=payload)
+        result = response.json()
 
         if result.get('success'):
-            encoded_password = os.environ.get('MAIL_PASSWORD')
-            password = base64.b64decode(encoded_password).decode()
-
-            subject = "Comentario en website"
-            name = request.form["name"]
-            email = request.form['email']
-            phone = request.form['phone']
-            msg = request.form["msg"]
-
-            server = smtplib.SMTP('smtp.gmail.com', 587)
-            server.starttls()
-            server.login("sitiowebbencomodentalclinic@gmail.com", password )
-
-            message  = MIMEText(f"subject: {subject}\n\nName: {name}\nEmail: {email}\nPhone: {phone}\nMessage: {msg}")
-
-            message["from"] = "sitiowebbencomodentalclinic@gmail.com"
-            message["to"] = "javier.rod.dev@gmail.com"
-            message["subject"] = subject
-
-            server.sendmail("sitiowebbencomodentalclinic@gmail.com", "javier.rod.dev@gmail.com", message.as_string())
-
-            server.quit()
-
-        return render_template('home.html')
-
+            return 'Formulario enviado'
+        else:
+            return "Error, turnstile failed"
+        
+    return 'end'
 
 
 @app.route('/web/mail/suggestions', methods=['GET', 'POST'])
