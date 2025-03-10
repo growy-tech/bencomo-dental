dc9412072222d7d0a454078d5d6b4a5e9c9f8df6
victorroddev <javier.rod.dev@gmail.com>
2025-03-10 10:19:31 -0600

    ADD recaptcha

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ import os
 import base64
 from email.mime.text import MIMEText
 
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, jsonify
 
 
 app = Flask(__name__)
@@ -16,31 +16,52 @@ def home():
 def email():
     encoded_password = os.environ.get('MAIL_PASSWORD')
     password = base64.b64decode(encoded_password).decode()
+    SECRET_KEY = "6Le4MvAqAAAAAEoI6ulWVwY9Yet6Lpjoy2gS9nWu"
+
     if request.method == 'POST':
+        respuesta_recaptcha = request.form.get('g-recaptcha-reponse')
         subject = "Comentario en sitio web de Bencomo" 
         name = request.form["name"]
         email = request.form["email"]
         phone = request.form["phone"]
         msg = request.form["msg"]
 
-        server = smtplib.SMTP('smtp.gmail.com', 587)
-        server.starttls()
-        server.login("sitiowebbencomodentalclinic@gmail.com", password )
+        if not respuesta_recaptcha:
+            return jsonify  ({'success': False, 'message': 'Por favor, complete el Captcha'})
+        
+        datos = {
+            'secret': SECRET_KEY,
+            'response': respuesta_recaptcha
+        }
 
-        message  = MIMEText(f"subject: {subject}\n\nName: {name}\nEmail: {email}\nPhone: {phone}\nMessage: {msg}")
+        respuesta = request.post('https://www.google.com/recaptcha/api/siteverify', data=datos)
+        resultado = respuesta.json()
 
-        message["from"] = "sitiowebbencomodentalclinic@gmail.com"
-        message["to"] = "hralvarez@bencomodentalclinic.com"
-        message["subject"] = subject
+        if resultado['sucess']:
+            subject = "Comentario en sitio web de Bencomo" 
+            name = request.form["name"]
+            email = request.form["email"]
+            phone = request.form["phone"]
+            msg = request.form["msg"]
 
-        server.sendmail("sitiowebbencomodentalclinic@gmail.com", "hralvarez@bencomodentalclinic.com", message.as_string())
+            server = smtplib.SMTP('smtp.gmail.com', 587)
+            server.starttls()
+            server.login("sitiowebbencomodentalclinic@gmail.com", password )
 
-        server.quit()
+            message  = MIMEText(f"subject: {subject}\n\nName: {name}\nEmail: {email}\nPhone: {phone}\nMessage: {msg}")
 
-        return render_template('home.html')
+            message["from"] = "sitiowebbencomodentalclinic@gmail.com"
+            message["to"] = "javier.rod.dev@gmail.com"
+            message["subject"] = subject
 
-    else:
-        return render_template('home.html')
+            server.sendmail("sitiowebbencomodentalclinic@gmail.com", "javier.rod.dev@gmail.com", message.as_string())
+
+            server.quit()
+
+            return render_template('home.html')
+
+        else:
+            return render_template('home.html')
 
 @app.route('/web/mail/suggestions', methods=['GET', 'POST'])
 def emailSuggestions():
@@ -60,10 +81,10 @@ def emailSuggestions():
         message  = MIMEText(f"subject: {subject}\n\nName: {name}\nEmail: {email}\nPhone: {phone}\nMessage: {msg}")
 
         message["from"] = "sitiowebbencomodentalclinic@gmail.com"
-        message["to"] = "hralvarez@bencomodentalclinic.com"
+        message["to"] = "javier.rod.dev@gmail.com"
         message["subject"] = subject
 
-        server.sendmail("sitiowebbencomodentalclinic@gmail.com", "hralvarez@bencomodentalclinic.com", message.as_string())
+        server.sendmail("sitiowebbencomodentalclinic@gmail.com", "javier.rod.dev@gmail.com", message.as_string())
 
         server.quit()
 
